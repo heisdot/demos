cbfaf80073053008dff2d949392b9f006ad84cff
coinko <lyh412661006>
2019-01-03 10:59:36 +0800

    swagger集成

--- src/main/java/com/sangxj/demos/DemosApplication.java
+++ src/main/java/com/sangxj/demos/DemosApplication.java
@@ -2,7 +2,9 @@ package com.sangxj.demos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2 //Enable swagger 2.0 spec
 @SpringBootApplication
 public class DemosApplication {
 
--- src/main/java/com/sangxj/demos/controller/Hello.java
+++ src/main/java/com/sangxj/demos/controller/Hello.java
@@ -1,7 +1,10 @@
 package com.sangxj.demos.controller;
 
 import com.sangxj.demos.vo.RestResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @创建时间 2019/1/3
  */
 @RestController
+@RequestMapping(value = "/hello", produces = "application/json;charset=utf-8")
+@Api("Hello")
 public class Hello {
     @GetMapping
+    @ApiOperation(value = "查询测试", notes = "查询测试1")
     public RestResult Hello(){
         return new RestResult("hello");
     }
