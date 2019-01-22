6bc8a9ff1da0801c68396ea01813d8c186cdb842
coinko <lyh412661006>
2019-01-22 11:09:09 +0800

    静态资源配置

---
+++ src/main/java/com/sangxj/demos/config/StaticResourceAppConfigurer.java
@@ -0,0 +1,24 @@
+package com.sangxj.demos.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by lijian on 2017/2/19.
+ */
+@Configuration
+public class StaticResourceAppConfigurer extends WebMvcConfigurerAdapter {
+
+    @Value("${web.upload-path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadPath);
+        super.addResourceHandlers(registry);
+    }
+
+}
